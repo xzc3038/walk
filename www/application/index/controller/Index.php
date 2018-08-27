ace363d1f427b650f605f2fd069eca4f04523b35
xzc <1805793040@qq.com>
2018-08-27 16:17:39 +0800

    update 16：17

--- www/application/index/controller/Index.php
+++ www/application/index/controller/Index.php
@@ -1,14 +1,13 @@
 <?php
+
 namespace app\index\controller;
 
-use app\model\User;
 use think\Controller;
 
 class Index extends Controller
 {
     public function index()
     {
-       $name = User::value('username');
-       return $name;
+      return $this->view->fetch();
     }
 }
